0960b3a4f228755967b0b03703a25ca8e5b03a07
hyeonji1125 <{guswl665@naver.com}>
2024-04-01 20:53:32 +0900

    test:테스트

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <p>현지 작성</p>
+        <p>화이팅!</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
