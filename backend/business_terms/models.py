a0836f7c689eba8184d3961ef83ae1a498cc114a
Samiiz <skboss159@gmail.com>
2024-04-02 23:23:42 +0900

    create business_terms model

---
+++ backend/business_terms/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from business_users.models import BusinessUser
+from terms.models import Term
+
+# Create your models here.
+
+class BusinessTerm(models.Model):
+    u = models.ForeignKey(BusinessUser, on_delete=models.CASCADE)
+    term_id = models.ForeignKey(Term, on_delete=models.CASCADE)
+    agreed_at = models.DateTimeField(auto_now_add=True)
