07f9ca28847ba8846774c1d4ce044760df229ccc
Samiiz <skboss159@gmail.com>
2024-04-02 23:24:52 +0900

    create users model

---
+++ backend/freelancer_users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from common.models import Common
+from countries.models import Country
+
+class FreelancerUser(AbstractBaseUser, Common):
+    id = models.AutoField(primary_key=True)
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_id = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=255)
+    country = models.ForeignKey(Country, on_delete=models.DO_NOTHING)
+    mobile = models.IntegerField()
+    email = models.EmailField(max_length=255, unique=True)
+    language = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'user_id'
+
+    def __str__(self):
+        return self.user_id
