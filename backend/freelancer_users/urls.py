359359bd9e03c21d3af59136215ded3f63481b2c
Samiiz <skboss159@gmail.com>
2024-04-07 09:59:38 +0900

    change the path

--- backend/freelancer_users/urls.py
+++ backend/freelancer_users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import UserDetail, SignUp
 
 urlpatterns = [
-    path('freelancer_user/<int:pk>/', UserDetail.as_view(), name='freelancer_user_detail'),
+    path('<int:pk>/', UserDetail.as_view(), name='freelancer_user_detail'),
     path('signup/', SignUp.as_view(), name='signup'),
 ]
