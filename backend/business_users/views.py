d4d0f13947585d319a22c2e7133e0d3f6bf52ff8
hjhj <hjhjgudwls@gmail.com>
2024-04-09 23:49:07 +0900

    create check existing password

--- backend/business_users/views.py
+++ backend/business_users/views.py
@@ -9,6 +9,7 @@ from business_emails.serializers import BusinessUserEmailVerification as EmailVe
 from .serializers import BusinessUserSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 class UserDetail(APIView):
     def get(self, request, pk):
@@ -73,19 +74,29 @@ class SignUp(APIView):
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({ "message" : str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    
+
 
 
 class ChangePasswordView(APIView):
     serializer_class = ChangePasswordSerializer
+    # permission_classes = [IsAuthenticated]  # 인증된 사용자만 비밀번호 변경을 허용
+    
     def put(self, request, pk):
         business_user = get_object_or_404(BusinessUser, pk=pk)
         serializer = ChangePasswordSerializer(data=request.data)
 
         if serializer.is_valid():
             new_password = serializer.validated_data.get('new_password')
-            business_user.set_password(new_password)
-            business_user.save()
-            return Response({'detail': '비밀번호가 성공적으로 변경되었습니다!'}, status=status.HTTP_200_OK)
+            old_password = request.data.get('old_password')  # 기존 비밀번호를 요청 데이터에서 가져옴.
+
+            # 기존 비밀번호와 신규 비밀번호가 일치하는지 확인.
+            if business_user.check_password(old_password):
+                # 일치할 경우에만 비밀번호를 변경하고 저장.
+                business_user.set_password(new_password)
+                business_user.save()
+                return Response({'detail': '비밀번호가 성공적으로 변경되었습니다.'}, status=status.HTTP_200_OK)
+            else:
+                # 일치하지 않을 경우 변경을 거부하고 에러 메시지를 반환.
+                return Response({'error': '기존 비밀번호가 일치하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
