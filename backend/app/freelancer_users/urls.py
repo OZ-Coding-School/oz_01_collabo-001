ed15c28cbcdcda7cec379704ae160aa89c4d8f01
Samiiz <skboss159@gmail.com>
2024-04-11 16:41:51 +0900

    modified urls & user check serializer

--- backend/app/freelancer_users/urls.py
+++ backend/app/freelancer_users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UserDetail, SignUp
+from .views import UserDetail, SignUp, Login
 from .views import UserDetail, ChangePasswordView
 
 urlpatterns = [
     path('<int:pk>/', UserDetail.as_view(), name='freelancer_user_detail'),
     path('signup/', SignUp.as_view(), name='signup'),
-    path('freelancer_user/<int:pk>/', UserDetail.as_view(), name='freelancer_user_detail'),
-    path('freelancer_user/<int:pk>/change_password/', ChangePasswordView.as_view(), name='change_password'),
+    path('<int:pk>/', UserDetail.as_view(), name='freelancer_user_detail'),
+    path('<int:pk>/change_password/', ChangePasswordView.as_view(), name='change_password'),
+    path('login/', Login.as_view(), name='login'),
 ]
