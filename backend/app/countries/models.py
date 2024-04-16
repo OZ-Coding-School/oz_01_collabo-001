ddd8c07216409473e2cd3a168e1401b6c72944ab
Samiiz <skboss159@gmail.com>
2024-04-17 03:06:33 +0900

    Add columns for English name, Korean name, and country number

--- backend/app/countries/models.py
+++ backend/app/countries/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 class Country(models.Model):
     id = models.AutoField(primary_key=True)
-    country_name = models.CharField(max_length=50)
-    code = models.CharField(max_length=50)
+    en_name = models.CharField(max_length=50)
+    kr_name = models.CharField(max_length=50)
+    code = models.CharField(max_length=3)
+    number = models.CharField(max_length=5)
