e1806f2f784bd0fb8abb358ced6f9428f4ea5f10
hyeonji1125 <{guswl665@naver.com}>
2024-04-25 17:32:08 +0900

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,38 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Nav from "./components/Nav";
-import CompleteRegistration from "./pages/CompleteRegistration";
-import Create from "./pages/Create";
-import DescribeYou from "./pages/DescribeYou";
-import EmployerSignupForm from "./pages/EmployerSignupForm";
-import Estimate from "./pages/Estimate";
-import HomeLogin from "./pages/HomeLogin";
-import Mypage from "./pages/Mypage";
-import PostProject from "./pages/Post-project";
-import Signup from "./pages/Signup";
-import KanbanPage from "./pages/kanban/KanbanPage";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Nav from './components/Nav';
+import CompleteRegistration from './pages/CompleteRegistration';
+import Create from './pages/Create';
+import DescribeYou from './pages/DescribeYou';
+import EmployerSignupForm from './pages/EmployerSignupForm';
+import Estimate from './pages/Estimate';
+import HomeLogin from './pages/HomeLogin';
+import Mypage from './pages/Mypage';
+import PostProject from './pages/Post-project';
+import Signup from './pages/Signup';
+import KanbanPage from './pages/kanban/KanbanPage';
 
 function App() {
   const queryClient = new QueryClient();
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <Nav />
-        <Routes>
-          <Route path="/login" element={<HomeLogin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route
-            path="/complete-registration"
-            element={<CompleteRegistration />}
-          />
-          <Route path="/describeyou" element={<DescribeYou />} />
-          <Route path="/" element={<Mypage />} />
-          <Route path="/kanban" element={<KanbanPage />} />
-          <Route path="/signup-employer" element={<EmployerSignupForm />} />
-          <Route path="/postproject" element={<PostProject />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/estimate" element={<Estimate />} />
-        </Routes>
+  <>
+  <QueryClientProvider client={queryClient}>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<HomeLogin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/complete-registration" element={<CompleteRegistration />} />
+        <Route path="/describeyou" element={<DescribeYou />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/kanban" element={<KanbanPage />} />
+        <Route path="/signup-employer" element={<EmployerSignupForm />} />
+        <Route path="/postproject" element={<PostProject />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/estimate" element={<Estimate />} />
+      </Routes>
       </QueryClientProvider>
-    </>
+      </>
   );
 }
 
