7d7655dbca1d02216a5296155736c1ea969e0dbb
Samiiz <skboss159@gmail.com>
2024-04-26 15:06:01 +0900

    hotfix: modified countries serializer

--- backend/app/countries/serializers.py
+++ backend/app/countries/serializers.py
@@ -7,17 +7,21 @@ class CountrySerializer(serializers.ModelSerializer):
         fields = ('id', 'en_name', 'kr_name', 'code', 'number')
 
 class SelectCountryENNameSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     en_name = serializers.CharField(max_length=50)
     code = serializers.CharField(max_length=3)
 
 class SelectCountryKRNameSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     kr_name = serializers.CharField(max_length=50)
     code = serializers.CharField(max_length=3)
 
 class SelectCountryENNumberSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     en_name = serializers.CharField(max_length=50)
     number = serializers.CharField(max_length=3)
 
 class SelectCountryKRNumberSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     kr_name = serializers.CharField(max_length=50)
     number = serializers.CharField(max_length=3)
