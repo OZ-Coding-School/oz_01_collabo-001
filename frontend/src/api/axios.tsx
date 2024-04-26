386d70b6c2b7272f8e58c391190e7ea866c17e47
kshenk00 <00byoon@gmail.com>
2024-04-27 06:15:37 +0900

    feat: 로그인마무리, 로그아웃메뉴 추가

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,11 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
 
-// const BASE_URL =  baseURL: 'http://3.37.128.177/';
-
-export default axios.create({
-    baseURL: 'http://3.37.128.177/'
-});
+// // const BASE_URL =  baseURL: 'http://3.37.128.177/';
 
 // export default axios.create({
-//   baseURL: BASE_URL
-// });ㄴ
+//     baseURL: 'http://3.37.128.177/'
+// });
+
+// // export default axios.create({
+// //   baseURL: BASE_URL
+// // });ㄴ
